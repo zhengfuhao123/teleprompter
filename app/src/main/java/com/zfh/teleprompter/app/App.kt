3bc61523ba3d3572e0bc87232c84287435681906
zhengfuhao <zhengfuhao@cvte.com>
2025-03-07 09:41:20 +0800

    [version] 1.0.2 [content] 接入OTA

--- app/src/main/java/com/zfh/teleprompter/app/App.kt
+++ app/src/main/java/com/zfh/teleprompter/app/App.kt
@@ -3,8 +3,12 @@ package com.zfh.teleprompter.app
 import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
+import com.tencent.upgrade.bean.UpgradeConfig
+import com.tencent.upgrade.core.DefaultUpgradeStrategyRequestCallback
+import com.tencent.upgrade.core.UpgradeManager
 import com.zfh.teleprompter.Options
 import com.zfh.teleprompter.utils.SPHelper
+
 
 class App : Application() {
 
@@ -30,6 +34,28 @@ class App : Application() {
         app = this
         SPHelper.init(this)
         Options.init()
+        initShiply()
+    }
+
+    // 初始化OTA组件
+    private fun initShiply() {
+        // 构造初始化参数
+        val config = UpgradeConfig.Builder()
+            .appId("2eaad712bf")
+            .appKey("e69e7fdc-50f4-46c4-a8af-e6bbf82f16ab")
+            .build()
+
+        // 初始化SDK
+        UpgradeManager.getInstance().init(this, config)
+
+        //App 首次启动时自动检查更新
+        UpgradeManager.getInstance()
+            .checkUpgrade(false, null, DefaultUpgradeStrategyRequestCallback())
+
+        //用户主动触发检查更新
+//        UpgradeManager.getInstance()
+//            .checkUpgrade(true, null, UpgradeReqCallbackForUserManualCheck())
+
     }
 
 }
